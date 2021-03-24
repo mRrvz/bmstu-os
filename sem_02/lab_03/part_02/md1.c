818c02341875fc42a26a3be90762f97c8604cc3b
romanov.alexey2000@gmail.com <romanov.alexey2000@gmail.com>
2021-03-24 18:50:58 +0300

    Lab03 was done

---
+++ sem_02/lab_03/part_02/md1.c
@@ -0,0 +1,27 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+#include "md.h"
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Romanov Alexey");
+
+char *md1_data = "Hello from md1!";
+
+extern char *md1_proc(void) { return md1_data; }
+
+extern char *md1_noexport(void) { return md1_data; }
+
+EXPORT_SYMBOL(md1_data);
+EXPORT_SYMBOL(md1_proc);
+
+static int __init md_init(void) {
+    printk("Module md1 was unloaded\n");
+
+    return 0;
+}
+
+static void __exit md_exit(void) { printk("Module md1 unloaded!\n"); }
+
+module_init(md_init);
+module_exit(md_exit);
---
+++ sem_02/lab_03/part_02/md2.c
@@ -0,0 +1,20 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+#include "md.h"
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Romanov Alexey");
+
+static int __init md_init(void) {
+    printk("Module md2 was loaded!\n");
+    printk("Value exported from md1 : %s\n", md1_data);
+    printk("Value returned md1_proc() is : %s\n", md1_proc());
+
+    return 0;
+}
+
+static void __exit md_exit(void) { printk("Module md2 unloaded!\n"); }
+
+module_init(md_init);
+module_exit(md_exit);
